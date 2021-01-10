7f7e8146f8d3dbe34f05738780c594c11ceaa7d6
Leonhard Brenner <lbrenner@execvision.io>
2021-01-09 21:25:29 -0500

    Removed clutter.

--- src/jsMain/kotlin/App.kt
+++ src/jsMain/kotlin/App.kt
@@ -7,9 +7,6 @@ import styled.styledDiv
 
 class App : RComponent<RProps, RState>() {
     private var tab1Value: Any = "one"
-    private var tab2Value: Any = 0
-    private var tab3Value: Any = 1
-    private var tab4Value: Any = 1
 
     private object CustomTabStyles : StyleSheet("ComponentStyles", isStatic = true) {
         val tabsRoot by css {
@@ -50,18 +47,6 @@ class App : RComponent<RProps, RState>() {
                 }
             }
 
-            fun RBuilder.customTab(label: String, value: Int) {
-                mTab(label, value) {
-                    css {
-                        +themeStyles.tabRoot
-                        if (tab3Value == value) {
-                            +themeStyles.tabSelected
-                        }
-                    }
-                    attrs.asDynamic().disableRipple = true
-                }
-            }
-
             styledDiv {
                 css { flexGrow = 1.0; backgroundColor = Color(theme.palette.background.paper) }
                 mAppBar(position = MAppBarPosition.static) {
@@ -82,71 +67,3 @@ class App : RComponent<RProps, RState>() {
 }
 
 fun RBuilder.app() = child(App::class) {}
-//import com.ccfraser.muirwik.components.*
-//import com.ccfraser.muirwik.components.button.mButton
-//import com.ccfraser.muirwik.components.styles.mStylesProvider
-//import kotlinx.css.*
-//import react.*
-//import styled.css
-//import styled.styledDiv
-//
-//
-//val App = functionalComponent<RProps> { _ ->
-//    val (tab1Value, setTab1Value) = useState<Any>("one")
-//
-//    //TODO - looks like this is how themes work
-//    //themeContext.Consumer { theme ->
-//    //    val themeStyles = object : StyleSheet("ComponentStyles", isStatic = true) {
-//    //        val tabRoot by css {
-//    //            textTransform = TextTransform.none
-//    //            fontWeight = FontWeight(theme.typography.fontWeightRegular.toString())
-//    //            marginRight = 4.spacingUnits
-//    //            hover {
-//    //                color = Color("#40a9ff")
-//    //                opacity = 1
-//    //            }
-//    //            focus {
-//    //                color = Color("#40a9ff")
-//    //            }
-//    //        }
-//    //        val tabSelected by css {
-//    //            color = Color("#1890ff")
-//    //            fontWeight = FontWeight(theme.typography.fontWeightMedium.toString())
-//    //        }
-//    //    }
-//    mStylesProvider("jss-insertion-point") {
-//        mThemeProvider {
-//            styledDiv {
-//                css { flexGrow = 1.0 }
-//                //css { flexGrow = 1.0; backgroundColor = Color(theme.palette.background.paper) }
-//                mAppBar(position = MAppBarPosition.static) {
-//                    mTabs(tab1Value, onChange = { _, value -> setTab1Value(value) }) {
-//                        mTab("Item One with a really long name for a tab", "one")
-//                        mTab("Item Two", "two")
-//                        mTab("Item Three", "three")
-//                    }
-//                }
-//                mTypography(paragraph = true) {
-//                    +"""$tab1Value This content is hidden and shown by use of the mCollapse control.""".trimIndent()
-//                }
-//                when (tab1Value) {
-//                    "one" -> { johnnySeeds() }
-//                    "two" -> { shoppingList() }
-//                    "three" -> { materialUIDemo() }
-//                }
-//            }
-//            //mButton("Click me to see a different app, count=$count!", onClick = { event ->
-//            //    setCount((count + 1) % 4) })
-//            ////TODO - look into hot-loader
-//            ////hotWrapper(app2())
-//            //+ ""
-//            //when (count) {
-//            //    0 -> { johnnySeeds() }
-//            //    1 -> { shoppingList() }
-//            //    2 -> { materialUIDemo() }
-//            //}
-//        }
-//    }
-//}
-//
-//fun RBuilder.app() = child(App)
