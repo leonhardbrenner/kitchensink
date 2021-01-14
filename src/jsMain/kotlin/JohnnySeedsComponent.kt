6897ae57558bd2529cb5f56f7833072acaed5a25
Leonhard Brenner <lbrenner@execvision.io>
2021-01-13 23:29:35 -0500

    main - Move the subcomponents withing the outer functional component. This keeps the namespace clean.

--- src/jsMain/kotlin/JohnnySeedsComponent.kt
+++ src/jsMain/kotlin/JohnnySeedsComponent.kt
@@ -1,15 +1,9 @@
 import com.ccfraser.muirwik.components.*
 import com.ccfraser.muirwik.components.menu.mMenuItem
 import kotlinext.js.jsObject
-import kotlinx.css.*
-import react.*
-import styled.StyleSheet
-import styled.css
-import styled.styledDiv
 import react.*
 import react.dom.*
 import kotlinx.coroutines.*
-import org.w3c.dom.events.Event
 
 private val scope = MainScope()
 
@@ -20,6 +14,78 @@ val JohnnySeedsComponent = functionalComponent<RProps> { _ ->
     inputProps.asDynamic().name = "age"
     inputProps.asDynamic().id = "age-simple"
 
+    val detailedSeed = functionalComponent<RProps> { _ ->
+        val (items, setItems) = useState(emptyList<JohnnySeeds.DetailedSeed>())
+
+        useEffect(dependencies = listOf()) {
+            scope.launch {
+                setItems(getJohnnySeedsDetailedSeed())
+            }
+        }
+
+        ul {
+            items.forEachIndexed { i, it ->
+                li {
+                    +"[$i] ${it.name}"
+                }
+            }
+        }
+    }
+
+    val category = functionalComponent<RProps> { _ ->
+        val (items, setItems) = useState(emptyList<JohnnySeeds.Category>())
+
+        useEffect(dependencies = listOf()) {
+            scope.launch {
+                setItems(getJohnnySeedsCategory())
+            }
+        }
+
+        ul {
+            items.forEachIndexed { i, it ->
+                li {
+                    +"[$i] ${it.name}"
+                }
+            }
+        }
+    }
+
+    val basicSeed = functionalComponent<RProps> { _ ->
+        val (items, setItems) = useState(emptyList<JohnnySeeds.BasicSeed>())
+
+        useEffect(dependencies = listOf()) {
+            scope.launch {
+                setItems(getJohnnySeedsBasicSeed())
+            }
+        }
+
+        ul {
+            items.forEachIndexed { i, it ->
+                li {
+                    +"[$i] ${it.name}"
+                }
+            }
+        }
+    }
+
+    val seedFacts = functionalComponent<RProps> { _ ->
+        val (items, setItems) = useState(emptyList<JohnnySeeds.SeedFacts>())
+
+        useEffect(dependencies = listOf()) {
+            scope.launch {
+                setItems(getJohnnySeedsSeedFacts())
+            }
+        }
+
+        ul {
+            items.forEachIndexed { i, it ->
+                li {
+                    +"[$i] ${it.name}"
+                }
+            }
+        }
+    }
+
     mSelect(age, name = "age", onChange = { event, _ -> setAge(event.targetValue) }) {
         attrs.inputProps = inputProps
         mMenuItem("Detailed Seed", value = JohnnySeeds.DetailedSeed.path)
@@ -28,87 +94,12 @@ val JohnnySeedsComponent = functionalComponent<RProps> { _ ->
         mMenuItem("Seed Fact", value = JohnnySeeds.SeedFacts.path)
     }
     when (age) {
-        JohnnySeeds.DetailedSeed.path -> detailedSeed()
-        JohnnySeeds.Category.path -> category()
-        JohnnySeeds.BasicSeed.path -> basicSeed()
-        JohnnySeeds.SeedFacts.path -> seedFacts()
+        JohnnySeeds.DetailedSeed.path -> child(detailedSeed)
+        JohnnySeeds.Category.path -> child(category)
+        JohnnySeeds.BasicSeed.path -> child(basicSeed)
+        JohnnySeeds.SeedFacts.path -> child(seedFacts)
     }
+
 }
 
 fun RBuilder.johnnySeeds() = child(JohnnySeedsComponent) {}
-
-val JohnnySeedsDetailedSeedComponent = functionalComponent<RProps> { _ ->
-    val (items, setItems) = useState(emptyList<JohnnySeeds.DetailedSeed>())
-
-    useEffect(dependencies = listOf()) {
-        scope.launch {
-            setItems(getJohnnySeedsDetailedSeed())
-        }
-    }
-
-    ul {
-        items.forEachIndexed { i, it ->
-            li {
-                +"[$i] ${it.name}"
-            }
-        }
-    }
-}
-fun RBuilder.detailedSeed() = child(JohnnySeedsDetailedSeedComponent) {}
-
-val JohnnySeedsCategoryComponent = functionalComponent<RProps> { _ ->
-    val (items, setItems) = useState(emptyList<JohnnySeeds.Category>())
-
-    useEffect(dependencies = listOf()) {
-        scope.launch {
-            setItems(getJohnnySeedsCategory())
-        }
-    }
-
-    ul {
-        items.forEachIndexed { i, it ->
-            li {
-                +"[$i] ${it.name}"
-            }
-        }
-    }
-}
-fun RBuilder.category() = child(JohnnySeedsCategoryComponent) {}
-
-val JohnnySeedsBasicSeedComponent = functionalComponent<RProps> { _ ->
-    val (items, setItems) = useState(emptyList<JohnnySeeds.BasicSeed>())
-
-    useEffect(dependencies = listOf()) {
-        scope.launch {
-            setItems(getJohnnySeedsBasicSeed())
-        }
-    }
-
-    ul {
-        items.forEachIndexed { i, it ->
-            li {
-                +"[$i] ${it.name}"
-            }
-        }
-    }
-}
-fun RBuilder.basicSeed() = child(JohnnySeedsBasicSeedComponent) {}
-
-val JohnnySeedsSeedFactsComponent = functionalComponent<RProps> { _ ->
-    val (items, setItems) = useState(emptyList<JohnnySeeds.SeedFacts>())
-
-    useEffect(dependencies = listOf()) {
-        scope.launch {
-            setItems(getJohnnySeedsSeedFacts())
-        }
-    }
-
-    ul {
-        items.forEachIndexed { i, it ->
-            li {
-                +"[$i] ${it.name}"
-            }
-        }
-    }
-}
-fun RBuilder.seedFacts() = child(JohnnySeedsSeedFactsComponent) {}
