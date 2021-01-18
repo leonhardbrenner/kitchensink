ae21547a1e43fe42c9402cd8cdd87652e820d42c
Leonhard Brenner <lbrenner@execvision.io>
2021-01-18 11:14:00 -0500

    main - First workings of a generator

--- src/jvmMain/kotlin/generators/Generator.kt
+++ src/jvmMain/kotlin/generators/Generator.kt
@@ -1,70 +1,8 @@
 package generators
 
-/**
- * Copied from: https://github.com/sufiannazim/kotlin-poet-dsl
- **/
 import com.squareup.kotlinpoet.*
 import java.io.StringWriter
 import kotlin.reflect.KClass
-
-class FileBuilder(packageName: String, fileName: String) {
-
-    private val file: FileSpec.Builder = FileSpec.builder(packageName, fileName)
-
-    fun clazz(name: String, block: ClazzBuilder.() -> Unit) {
-        file.addType(ClazzBuilder(TypeSpec.classBuilder(name)).apply(block).build())
-    }
-
-    fun function(name: String, block: FunctionBuilder.() -> Unit) {
-        file.addFunction(FunctionBuilder(FunSpec.builder(name)).apply(block).build())
-    }
-
-    fun build(): FileSpec {
-        return file.build()
-    }
-}
-
-class ClazzBuilder(private val clazz: TypeSpec.Builder) {
-
-    fun primaryConstructor(block: FunctionBuilder.() -> Unit) {
-        clazz.primaryConstructor(FunctionBuilder(FunSpec.constructorBuilder()).apply(block).build())
-    }
-
-    fun annotation(type: KClass<*>, block: AnnotationSpec.Builder.() -> Unit) {
-        clazz.addAnnotation(AnnotationSpec.builder(type.asClassName()).apply(block).build())
-    }
-
-    fun property(name: String, type: KClass<*>, block: PropertySpec.Builder.() -> Unit = {}) {
-        clazz.addProperty(PropertySpec.builder(name, type).apply(block).build())
-    }
-
-    fun function(name: String, block: FunctionBuilder.() -> Unit) {
-        clazz.addFunction(FunctionBuilder(FunSpec.builder(name)).apply(block).build())
-    }
-
-    fun build(): TypeSpec {
-        return clazz.build()
-    }
-}
-
-class FunctionBuilder(private val function: FunSpec.Builder) {
-
-    fun parameter(name: String, type: KClass<*>, block: ParameterSpec.Builder.() -> Unit = {}) {
-        function.addParameter(ParameterSpec.builder(name, type).apply(block).build())
-    }
-
-    fun body(block: CodeBlock.Builder.() -> Unit) {
-        function.addCode(CodeBlock.builder().apply(block).build())
-    }
-
-    fun annotation(type: KClass<*>) {
-        function.addAnnotation(type)
-    }
-
-    fun build(): FunSpec {
-        return function.build()
-    }
-}
 
 @DslMarker
 @Target(AnnotationTarget.ANNOTATION_CLASS, AnnotationTarget.TYPE)
@@ -73,42 +11,151 @@ annotation class KotlinPoetDsl
 fun kotlinFile(packageName: String, fileName: String, block: (@KotlinPoetDsl FileBuilder).() -> Unit): FileSpec {
     return FileBuilder(packageName, fileName).apply(block).build()
 }
+class FileBuilder(packageName: String, fileName: String) {
+
+    private val builder: FileSpec.Builder = FileSpec.builder(packageName, fileName)
+
+    inner class Class(name: String, block: ClazzBuilder.() -> Unit) {
+        init {
+            builder.addType(ClazzBuilder(TypeSpec.classBuilder(name)).apply(block).build())
+        }
+    }
+
+    inner class Interface(val name: String, val block: InterfaceBuilder.() -> Unit) {
+        init {
+            builder.addType(InterfaceBuilder(TypeSpec.interfaceBuilder(name)).apply(block).build())
+        }
+    }
+
+    inner class Function(name: String, block: FunctionBuilder.() -> Unit) {
+        init {
+            builder.addFunction(FunctionBuilder(FunSpec.builder(name)).apply(block).build())
+        }
+    }
+
+    fun build(): FileSpec {
+        return builder.build()
+    }
+}
 
 fun kotlinClass(name: String, block: (@KotlinPoetDsl ClazzBuilder).() -> Unit) : TypeSpec {
     return ClazzBuilder(TypeSpec.classBuilder(name)).apply(block).build()
+}
+
+class ClazzBuilder(private val builder: TypeSpec.Builder) {
+
+    inner class PrimaryConstructor(block: FunctionBuilder.() -> Unit) {
+        init {
+            builder.primaryConstructor(FunctionBuilder(FunSpec.constructorBuilder()).apply(block).build())
+        }
+    }
+
+    inner class Annotation(type: KClass<*>, block: AnnotationSpec.Builder.() -> Unit) {
+        init {
+            builder.addAnnotation(AnnotationSpec.builder(type.asClassName()).apply(block).build())
+        }
+    }
+
+    inner class Property(name: String, type: KClass<*>, block: PropertySpec.Builder.() -> Unit = {}) {
+        init {
+            builder.addProperty(PropertySpec.builder(name, type).apply(block).build())
+        }
+    }
+
+    inner class Function(name: String, block: FunctionBuilder.() -> Unit) {
+        init {
+            builder.addFunction(FunctionBuilder(FunSpec.builder(name)).apply(block).build())
+        }
+    }
+
+    fun build(): TypeSpec {
+
+        return builder.build()
+    }
+}
+
+class InterfaceBuilder(private val builder: TypeSpec.Builder) {
+
+    inner class Class(name: String, block: ClazzBuilder.() -> Unit) {
+        init {
+            builder.addType(ClazzBuilder(TypeSpec.classBuilder(name)).apply(block).build())
+        }
+    }
+
+    inner class DataClass(
+        type: KClass<*>,
+        modifiers: List<KModifier> = emptyList(),
+        block: ClazzBuilder.() -> Unit
+    ) {
+        init {
+            builder.addType(ClazzBuilder(TypeSpec.classBuilder(type.simpleName!!)
+                .addSuperinterface(type)
+                .addModifiers(modifiers)).apply(block).build())
+        }
+    }
+
+    fun build(): TypeSpec {
+        return builder.build()
+    }
+
 }
 
 fun kotlinFunction(name: String, block: (@KotlinPoetDsl FunctionBuilder).() -> Unit) : FunSpec {
     return FunctionBuilder(FunSpec.builder(name)).apply(block).build()
 }
 
+class FunctionBuilder(private val function: FunSpec.Builder) {
+
+    inner class Parameter(name: String, type: KClass<*>, block: ParameterSpec.Builder.() -> Unit = {}) {
+        init {
+            function.addParameter(ParameterSpec.builder(name, type).apply(block).build())
+        }
+    }
+
+    inner class Body(block: CodeBlock.Builder.() -> Unit) {
+        init {
+            function.addCode(CodeBlock.builder().apply(block).build())
+        }
+    }
+
+    inner class Annotation(type: KClass<*>) {
+        init {
+            function.addAnnotation(type)
+        }
+    }
+
+    fun build(): FunSpec {
+        return function.build()
+    }
+}
+
 fun main(args: Array<String>) {
     val generatedCode = StringWriter().apply {
         kotlinFile("this.is.my.package", "HelloWorld") {
 
-            clazz("Greeter") {
+            Class("Greeter") {
 
-                primaryConstructor {
-                    parameter("name", String::class)
+                PrimaryConstructor {
+                    Parameter("name", String::class)
                 }
 
-                property("name", String::class) {
+                Property("name", String::class) {
                     initializer("name")
                 }
 
-                function("greet") {
-                    body {
+                Function("greet") {
+                    Body {
                         addStatement("println(%P)", "Hello, \$name")
                     }
                 }
             }
 
-            function("main") {
-                parameter("args", String::class) {
+            Function("main") {
+                Parameter("args", String::class) {
                     addModifiers(KModifier.VARARG)
                 }
 
-                body {
+                Body {
                     addStatement("%T(args[0]).greet()", ClassName("", "Greeter"))
                 }
             }
--- src/jvmMain/kotlin/generators/ReflectionUtils.kt
+++ src/jvmMain/kotlin/generators/ReflectionUtils.kt
@@ -1,11 +1,14 @@
 package generators
 
 import JohnnySeeds
+import com.squareup.kotlinpoet.ClassName
+import com.squareup.kotlinpoet.KModifier
 import com.squareup.kotlinpoet.asTypeName
+import java.io.StringWriter
 import kotlin.reflect.KCallable
 import kotlin.reflect.KClass
 
-class ComplexType <T: Any> (private val manifest: KClass<T>) {
+class ComplexType <T: Any> (val manifest: KClass<T>) {
     val description = manifest.simpleName + " - " + manifest.qualifiedName
     val complexTypes
     get() = manifest.nestedClasses.map { ComplexType(it) }
@@ -20,7 +23,7 @@ class ComplexType <T: Any> (private val manifest: KClass<T>) {
 
 
 }
-class SimpleType (private val manifest: KCallable<*>) {
+class SimpleType (val manifest: KCallable<*>) {
     val description
     get() = "    ${manifest.name}, ${manifest.returnType.asTypeName()}"
 }
@@ -41,4 +44,43 @@ fun main(args: Array<String>) {
         //TODO - investigate I find it odd that the outer interface does not have these.
         println(y.description)
     }
+
+    val generatedCode = StringWriter().apply {
+        val rootName = "JohnnySeedsDto"
+        kotlinFile("this.is.my.package", rootName) {
+
+            Interface(rootName) {
+                reflector.complexTypes.forEach {
+                    DataClass(it.manifest, modifiers = listOf(KModifier.DATA)) {
+
+                    }
+                }
+                //primaryConstructor {
+                //    parameter("name", String::class)
+                //}
+                //
+                //property("name", String::class) {
+                //    initializer("name")
+                //}
+                //
+                //function("greet") {
+                //    body {
+                //        addStatement("println(%P)", "Hello, \$name")
+                //    }
+                //}
+            }
+
+            Function("main") {
+                Parameter("args", String::class) {
+                    addModifiers(KModifier.VARARG)
+                }
+
+                Body {
+                    addStatement("%T(args[0]).greet()", ClassName("", "Greeter"))
+                }
+            }
+
+        }.writeTo(this)
+    }.toString()
+    println(generatedCode)
 }
